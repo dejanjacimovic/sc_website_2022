22b0b677ddd1d2d4b74bacdf3b4059164010cb63
Dejan Jacimovic <dejan@stuntcoders.com>
2022-01-14 22:40:13 +0100

    Update layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './css/tailwind.css';
 
-export default function Layout(props) {
+export default function Layout(content) {
   return (
     <div>
-      {props.title}
+      {content}
 
       <div className="bg-gray-50">
         <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
