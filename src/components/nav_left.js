a3ac5a45870db841829af5869f4a487fba4d2039
Dejan Jacimovic <dejan@stuntcoders.com>
2022-01-14 22:06:43 +0100

    Fix alignment

--- src/components/nav_left.js
+++ src/components/nav_left.js
@@ -9,7 +9,7 @@ export default function NavLeft() {
         <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
           <div className="flex items-center justify-between w-full md:w-auto">
             <a href="/" aria-label="Home">
-                <img className="h-10 w-auto sm:h-12" src="https://stcdshare.s3.amazonaws.com/RAsTG0DklW.png" loading="auto" alt=""/>
+                <img className="h-10 w-auto sm:h-12" src="https://stcdshare.s3.amazonaws.com/RAsTG0DklW.png" loading="auto" alt="" />
             </a>
             <div className="-mr-2 flex items-center md:hidden">
               <button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out" id="main-menu" type="button" aria-label="Main menu" aria-haspopup="true" onclick="document.getElementById('main-mobile-menu').className='absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden z-50'">
