b91e3bfb0c5f642a048985c3a8645d5d9d8ed49f
Dejan Jacimovic <dejan@stuntcoders.com>
2022-01-16 17:48:29 +0100

    Update classes names

--- src/pages/privacy-policy.jsx
+++ src/pages/privacy-policy.jsx
@@ -3,7 +3,7 @@ import { Link } from 'gatsby';
 import LayoutPage from '../components/layout_page';
 import NavCenter from '../components/nav_center';
 
-export default function CancellationPolicy() {
+export default function PrivacyPolicy() {
   return (
     <LayoutPage>
       <div class="relative pt-6 pb-12 sm:pb-16 md:pb-20 lg:pb-28 xl:pb-32">
