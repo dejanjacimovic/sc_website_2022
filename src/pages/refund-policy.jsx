b6942b7af8ebe32ba04d27f232cdc94d97639231
Dejan Jacimovic <dejan@stuntcoders.com>
2022-01-16 17:43:04 +0100

    Add refund policy

---
+++ src/pages/refund-policy.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Link } from 'gatsby';
+import LayoutPage from '../components/layout_page';
+import NavCenter from '../components/nav_center';
+
+export default function CancellationPolicy() {
+  return (
+    <LayoutPage>
+      <div class="relative pt-6 pb-12 sm:pb-16 md:pb-20 lg:pb-28 xl:pb-32">
+        <NavCenter />
+      </div>
+    </LayoutPage>
+  );
+}
