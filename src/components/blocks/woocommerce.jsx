ffaf7615ec627e4fc2d37f1e08ce64293a960651
Dejan Jacimovic <dejan@stuntcoders.com>
2022-01-17 14:47:39 +0100

    Add content to woocommerce block

--- src/components/blocks/woocommerce.jsx
+++ src/components/blocks/woocommerce.jsx
@@ -2,5 +2,178 @@ import React from 'react';
 
 export default function Woocommerce() {
   return (
-    )
+    <div class="relative bg-white py-16 sm:py-24 lg:py-32">
+      <div class="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
+        <h2 class="text-base font-semibold tracking-wider text-indigo-600 uppercase">
+          Does your business run on
+        </h2>
+        <p class="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
+          WooCommerce?
+        </p>
+        <p class="mt-5 max-w-prose mx-auto text-xl text-gray-500">
+          It has been more than 9 years since we dived into the WooCommerce
+          world, only one year after it was born. So, we have plenty to offer
+          regarding support and development. We know how to solve and integrate
+          all important segments of your e-commerce business.
+        </p>
+        <p class="mt-5 max-w-prose mx-auto text-xl text-gray-500">
+          Some of the areas we can help you with are listed below.
+        </p>
+        <div class="mt-12 text-left">
+          <div class="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-2">
+            <div class="pt-6">
+              <div class="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+                <div class="-mt-6">
+                  <div>
+                    <span class="inline-flex items-center justify-center p-3 bg-indigo-500 rounded-md shadow-lg">
+                      <svg
+                        class="h-6 w-6 text-white"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
+                        ></path>
+                      </svg>
+                    </span>
+                  </div>
+                  <h3 class="mt-8 text-lg font-medium text-gray-900 tracking-tight">
+                    Shipping
+                  </h3>
+                  <p class="mt-5 text-base text-gray-500">
+                    <ul class="mt-2 text-base leading-6 text-gray-500 list-disc pl-4">
+                      <li>Porterbuddy</li>
+                      <li>Consignor</li>
+                      <li>Posten</li>
+                      <li>Postnord</li>
+                      <li>Bring</li>
+                      <li>Best Transport</li>
+                      <li>Helthjem</li>
+                    </ul>
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div class="pt-6">
+              <div class="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+                <div class="-mt-6">
+                  <div>
+                    <span class="inline-flex items-center justify-center p-3 bg-indigo-500 rounded-md shadow-lg">
+                      <svg
+                        class="h-6 w-6 text-white"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"
+                        ></path>
+                      </svg>
+                    </span>
+                  </div>
+                  <h3 class="mt-8 text-lg font-medium text-gray-900 tracking-tight">
+                    Catalog Management
+                  </h3>
+                  <p class="mt-5 text-base text-gray-500">
+                    <ul class="mt-2 text-base leading-6 text-gray-500 list-disc pl-4">
+                      <li>Virtual products</li>
+                      <li>Downloadable/digital products with samples</li>
+                      <li>Backordered inventory items</li>
+                      <li>Advanced pricing rules</li>
+                      <li>Improved search results</li>
+                      <li>Customer personalised products</li>
+                      <li>Multi-tier upsell</li>
+                    </ul>
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div class="pt-6">
+              <div class="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+                <div class="-mt-6">
+                  <div>
+                    <span class="inline-flex items-center justify-center p-3 bg-indigo-500 rounded-md shadow-lg">
+                      <svg
+                        class="h-6 w-6 text-white"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+                        ></path>
+                      </svg>
+                    </span>
+                  </div>
+                  <h3 class="mt-8 text-lg font-medium text-gray-900 tracking-tight">
+                    ERP, CRM, Accounting
+                  </h3>
+                  <p class="mt-5 text-base text-gray-500">
+                    <ul class="mt-2 text-base leading-6 text-gray-500 list-disc pl-4">
+                      <li>Tripletex</li>
+                      <li>Visma Global</li>
+                      <li>Visma One</li>
+                      <li>SuperOffice</li>
+                      <li>Microsoft Navision</li>
+                      <li>SAP</li>
+                    </ul>
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div class="pt-6">
+              <div class="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+                <div class="-mt-6">
+                  <div>
+                    <span class="inline-flex items-center justify-center p-3 bg-indigo-500 rounded-md shadow-lg">
+                      <svg
+                        class="h-6 w-6 text-white"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M7 12l3-3 3 3 4-4M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z"
+                        ></path>
+                      </svg>
+                    </span>
+                  </div>
+                  <h3 class="mt-8 text-lg font-medium text-gray-900 tracking-tight">
+                    Reporting
+                  </h3>
+                  <p class="mt-5 text-base text-gray-500">
+                    <ul class="mt-2 text-base leading-6 text-gray-500 list-disc pl-4">
+                      <li>Tax reports</li>
+                      <li>Abandoned shopping cart</li>
+                      <li>Low stock alert</li>
+                      <li>Popular items / search</li>
+                      <li>Product reviews</li>
+                      <li>Coupon usage</li>
+                      <li>Total returns / refunds</li>
+                    </ul>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
