fecf662d92b41b4e4acd87322d704ea121edfa7a
Dejan Jacimovic <dejan@stuntcoders.com>
2022-01-17 21:33:45 +0100

    Update layout page component to make more sense

--- src/components/layout_page.js
+++ src/components/layout_page.js
@@ -1,13 +1,18 @@
+import './css/tailwind.css';
 import React from 'react';
 import Header from '../components/header';
-import './css/tailwind.css';
+import NavCenter from '../components/nav_center';
+import NavLeft from '../components/nav_left';
 
 export default function LayoutPage(content) {
   return (
     (<Header />),
     (
       <body className="font-sans antialiased text-gray-900">
-        <div className="">{content.children}</div>
+        <div className="">
+          <Hero nav=content.nav />
+          {content.children}
+        </div>
       </body>
     )
   );
