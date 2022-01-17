ff694e82069dd1c346d3bec945818407a15bcc99
Dejan Jacimovic <dejan@stuntcoders.com>
2022-01-17 21:58:03 +0100

    Hero JSX

---
+++ src/components/hero.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import NavCenter from '../components/nav_center';
+import NavLeft from '../components/nav_left';
+
+export default function Hero(content) {
+  return <NavCenter />;
+}
