7430338289f002e0903918385a511d39d6c4f4f7
Dejan Jacimovic <dejan@stuntcoders.com>
2022-01-19 00:12:49 +0100

    Make nav center white without dots

--- src/components/nav_center.jsx
+++ src/components/nav_center.jsx
@@ -4,76 +4,8 @@ import LiveAndBreatheEcommerce from '../components/heroes/live_and_breathe_ecomm
 
 export default function NavCenter() {
   return (
-    <div className="relative bg-gray-50 overflow-hidden">
-      <div className="hidden sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full">
-        <div className="relative h-full max-w-screen-xl mx-auto">
-          <svg
-            className="absolute right-full transform translate-y-1/4 translate-x-1/4 lg:translate-x-1/2"
-            width="404"
-            height="784"
-            fill="none"
-            viewBox="0 0 404 784"
-          >
-            <defs>
-              <pattern
-                id="f210dbf6-a58d-4871-961e-36d5016a0f49"
-                x="0"
-                y="0"
-                width="20"
-                height="20"
-                patternUnits="userSpaceOnUse"
-              >
-                <rect
-                  className="text-gray-200"
-                  x="0"
-                  y="0"
-                  width="4"
-                  height="4"
-                  fill="currentColor"
-                ></rect>
-              </pattern>
-            </defs>
-            <rect
-              width="404"
-              height="784"
-              fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"
-            ></rect>
-          </svg>
-          <svg
-            className="absolute left-full transform -translate-y-3/4 -translate-x-1/4 md:-translate-y-1/2 lg:-translate-x-1/2"
-            width="404"
-            height="784"
-            fill="none"
-            viewBox="0 0 404 784"
-          >
-            <defs>
-              <pattern
-                id="5d0dd344-b041-4d26-bec4-8d33ea57ec9b"
-                x="0"
-                y="0"
-                width="20"
-                height="20"
-                patternUnits="userSpaceOnUse"
-              >
-                <rect
-                  className="text-gray-200"
-                  x="0"
-                  y="0"
-                  width="4"
-                  height="4"
-                  fill="currentColor"
-                ></rect>
-              </pattern>
-            </defs>
-            <rect
-              width="404"
-              height="784"
-              fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)"
-            ></rect>
-          </svg>
-        </div>
-      </div>
-      <div className="relative pt-6 pb-12 sm:pb-16 md:pb-20 lg:pb-28 xl:pb-32">
+    <div className="relative overflow-hidden">
+      <div className="relative pt-6 pb-6">
         <div className="max-w-screen-xl mx-auto px-4 sm:px-6">
           <nav className="relative flex items-center justify-between sm:h-10 md:justify-center">
             <div className="flex items-center flex-1 md:absolute md:inset-y-0 md:left-0">
