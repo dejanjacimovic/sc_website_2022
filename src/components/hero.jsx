f79a03945f5577b29b33f658f883066b1d3d28dc
Dejan Jacimovic <dejan@stuntcoders.com>
2022-01-19 22:16:14 +0100

    Remove heroas due to change of approach

--- src/components/hero.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import NavCenter from '../components/nav_center';
-import NavLeft from '../components/nav_left';
-
-export default function Hero(content) {
-  return <NavCenter />;
-}
--- src/components/layout_page.jsx
+++ src/components/layout_page.jsx
@@ -1,7 +1,6 @@
 import './css/tailwind.css';
 import React from 'react';
 import Header from './header';
-import Hero from './hero';
 import Footer from './footer';
 
 export default function LayoutPage(content) {
