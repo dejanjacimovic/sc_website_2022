50f5060edfa0637708ddecd0d322b8a222c07a37
Dejan Jacimovic <dejan@stuntcoders.com>
2022-01-19 22:16:41 +0100

    Update class names on nav center

--- src/components/nav_center.jsx
+++ src/components/nav_center.jsx
@@ -3,20 +3,20 @@ import React from 'react';
 export default function NavCenter() {
   return (
     (
-      <div class="max-w-screen-xl mx-auto px-4 sm:px-6">
-        <nav class="relative flex items-center justify-between sm:h-10 md:justify-center">
-          <div class="flex items-center flex-1 md:absolute md:inset-y-0 md:left-0">
-            <div class="flex items-center justify-between w-full md:w-auto">
+      <div className="max-w-screen-xl mx-auto px-4 sm:px-6">
+        <nav className="relative flex items-center justify-between sm:h-10 md:justify-center">
+          <div className="flex items-center flex-1 md:absolute md:inset-y-0 md:left-0">
+            <div className="flex items-center justify-between w-full md:w-auto">
               <a href="/" aria-label="Home">
                 <img
-                  class="h-8 w-auto sm:h-12"
+                  className="h-8 w-auto sm:h-12"
                   src="https://stcdshare.s3.amazonaws.com/RAsTG0DklW.png"
                   alt="StuntCoders logo"
                 />
               </a>
-              <div class="-mr-2 flex items-center md:hidden">
+              <div className="-mr-2 flex items-center md:hidden">
                 <button
-                  class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
+                  className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
                   id="main-menu"
                   type="button"
                   aria-label="Main menu"
@@ -24,7 +24,7 @@ export default function NavCenter() {
                   onclick="document.getElementById('main-mobile-menu').className='absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden z-50'"
                 >
                   <svg
-                    class="h-6 w-6"
+                    className="h-6 w-6"
                     stroke="currentColor"
                     fill="none"
                     viewBox="0 0 24 24"
@@ -40,21 +40,21 @@ export default function NavCenter() {
               </div>
             </div>
           </div>
-          <div class="hidden md:flex md:space-x-10">
-            <div class="relative">
+          <div className="hidden md:flex md:space-x-10">
+            <div className="relative">
               <button
                 type="button"
-                class="text-gray-500 group rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none"
+                className="text-gray-500 group rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none"
                 aria-expanded="false"
               >
                 <span
-                  class="font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out"
+                  className="font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out"
                   onclick="document.getElementById('service-submenu').classList.toggle('hidden')"
                 >
                   Services
                 </span>
                 <svg
-                  class="text-gray-400 ml-2 h-5 w-5 group-hover:text-gray-500"
+                  className="text-gray-400 ml-2 h-5 w-5 group-hover:text-gray-500"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 20 20"
                   fill="currentColor"
@@ -71,30 +71,30 @@ export default function NavCenter() {
 
               <div
                 id="service-submenu"
-                class="hidden absolute z-10 left-1/2 transform -translate-x-1/2 mt-3 px-2 w-screen max-w-xs sm:px-0 z-50"
+                className="hidden absolute z-10 left-1/2 transform -translate-x-1/2 mt-3 px-2 w-screen max-w-xs sm:px-0 z-50"
               >
-                <div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                  <div class="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                  <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
                     <a
                       href="/services-woocommerce-maintenance/"
-                      class="-m-3 p-3 block rounded-md hover:bg-gray-50 transition ease-in-out duration-150"
+                      className="-m-3 p-3 block rounded-md hover:bg-gray-50 transition ease-in-out duration-150"
                     >
-                      <p class="text-base font-medium text-gray-900">
+                      <p className="text-base font-medium text-gray-900">
                         WooCommerce Maintenance
                       </p>
-                      <p class="mt-1 text-sm text-gray-500">
+                      <p className="mt-1 text-sm text-gray-500">
                         Learn about unlimited WordPress support.
                       </p>
                     </a>
 
                     <a
                       href="/services-ecommerce-development/"
-                      class="-m-3 p-3 block rounded-md hover:bg-gray-50 transition ease-in-out duration-150"
+                      className="-m-3 p-3 block rounded-md hover:bg-gray-50 transition ease-in-out duration-150"
                     >
-                      <p class="text-base font-medium text-gray-900">
+                      <p className="text-base font-medium text-gray-900">
                         eCommerce Development
                       </p>
-                      <p class="mt-1 text-sm text-gray-500">
+                      <p className="mt-1 text-sm text-gray-500">
                         Get all of your questions answered about eCommerce
                         development.
                       </p>
@@ -102,12 +102,12 @@ export default function NavCenter() {
 
                     <a
                       href="/services-b2b-ecommerce/"
-                      class="-m-3 p-3 block rounded-md hover:bg-gray-50 transition ease-in-out duration-150"
+                      className="-m-3 p-3 block rounded-md hover:bg-gray-50 transition ease-in-out duration-150"
                     >
-                      <p class="text-base font-medium text-gray-900">
+                      <p className="text-base font-medium text-gray-900">
                         B2B eCommerce
                       </p>
-                      <p class="mt-1 text-sm text-gray-500">
+                      <p className="mt-1 text-sm text-gray-500">
                         The eCommerce solution for all your B2B needs.
                       </p>
                     </a>
@@ -116,34 +116,34 @@ export default function NavCenter() {
               </div>
             </div>
             <a
-              class="font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out"
+              className="font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out"
               href="/testimonials/"
             >
               Testimonials
             </a>
             <a
-              class="font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out"
+              className="font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out"
               href="/portfolio/"
             >
               Portfolio
             </a>
             <a
-              class="font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out"
+              className="font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out"
               href="/pricing/"
             >
               Pricing
             </a>
             <a
-              class="font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out"
+              className="font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out"
               href="/about-us/"
             >
               About us
             </a>
           </div>
-          <div class="hidden md:absolute md:flex md:items-center md:justify-end md:inset-y-0 md:right-0">
-            <span class="inline-flex rounded-md shadow">
+          <div className="hidden md:absolute md:flex md:items-center md:justify-end md:inset-y-0 md:right-0">
+            <span className="inline-flex rounded-md shadow">
               <a
-                class="inline-flex items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-600 bg-white hover:text-indigo-500 focus:outline-none focus:border-indigo-300 focus:shadow-outline-indigo active:bg-gray-50 active:text-indigo-700 transition duration-150 ease-in-out"
+                className="inline-flex items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-600 bg-white hover:text-indigo-500 focus:outline-none focus:border-indigo-300 focus:shadow-outline-indigo active:bg-gray-50 active:text-indigo-700 transition duration-150 ease-in-out"
                 href="/contact-us/"
               >
                 Contact us
@@ -155,33 +155,33 @@ export default function NavCenter() {
     ),
     (
       <div
-        class="hidden absolute top-0 inset-x-0 p-2 transition transform origin-top-right z-50"
+        className="hidden absolute top-0 inset-x-0 p-2 transition transform origin-top-right z-50"
         id="main-mobile-menu"
       >
-        <div class="rounded-lg shadow-md">
+        <div className="rounded-lg shadow-md">
           <div
-            class="rounded-lg bg-white shadow-xs overflow-hidden"
+            className="rounded-lg bg-white shadow-xs overflow-hidden"
             role="menu"
             aria-orientation="vertical"
             aria-labelledby="main-menu"
           >
-            <div class="px-5 pt-4 flex items-center justify-between">
+            <div className="px-5 pt-4 flex items-center justify-between">
               <div>
                 <img
-                  class="h-8 w-auto"
+                  className="h-8 w-auto"
                   src="https://stcdshare.s3.amazonaws.com/RAsTG0DklW.png"
                   alt="StuntCoders logo"
                 />
               </div>
-              <div class="-mr-2">
+              <div className="-mr-2">
                 <button
-                  class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
+                  className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
                   type="button"
                   aria-label="Close menu"
                   onclick="document.getElementById('main-mobile-menu').className='hidden absolute top-0 inset-x-0 p-2 transition transform origin-top-right z-50'"
                 >
                   <svg
-                    class="h-6 w-6"
+                    className="h-6 w-6"
                     stroke="currentColor"
                     fill="none"
                     viewBox="0 0 24 24"
@@ -196,19 +196,19 @@ export default function NavCenter() {
                 </button>
               </div>
             </div>
-            <div class="px-2 pt-2 pb-3">
-              <div class="relative">
+            <div className="px-2 pt-2 pb-3">
+              <div className="relative">
                 <button
                   type="button"
-                  class="w-full px-3 py-2 text-gray-500 group rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 hover:bg-gray-50 focus:outline-none"
+                  className="w-full px-3 py-2 text-gray-500 group rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 hover:bg-gray-50 focus:outline-none"
                   aria-expanded="false"
                   onclick="document.getElementById('service-submenu-mobile').classList.toggle('hidden')"
                 >
-                  <span class="font-medium text-gray-700 hover:text-gray-900  focus:outline-none focus:text-gray-900 transition duration-150 ease-in-out">
+                  <span className="font-medium text-gray-700 hover:text-gray-900  focus:outline-none focus:text-gray-900 transition duration-150 ease-in-out">
                     Services
                   </span>
                   <svg
-                    class="text-gray-400 ml-2 h-5 w-5 group-hover:text-gray-500"
+                    className="text-gray-400 ml-2 h-5 w-5 group-hover:text-gray-500"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                     fill="currentColor"
@@ -224,30 +224,30 @@ export default function NavCenter() {
 
                 <div
                   id="service-submenu-mobile"
-                  class="hidden px-2 w-screen max-w-xl sm:px-0"
+                  className="hidden px-2 w-screen max-w-xl sm:px-0"
                 >
-                  <div class="overflow-hidden">
-                    <div class="relative grid gap-6 px-5 sm:gap-8 sm:p-8">
+                  <div className="overflow-hidden">
+                    <div className="relative grid gap-6 px-5 sm:gap-8 sm:p-8">
                       <a
                         href="/services-woocommerce-maintenance/"
-                        class="-m-3 p-3 block rounded-md hover:bg-gray-50 transition ease-in-out duration-150"
+                        className="-m-3 p-3 block rounded-md hover:bg-gray-50 transition ease-in-out duration-150"
                       >
-                        <p class="text-base font-medium text-gray-900">
+                        <p className="text-base font-medium text-gray-900">
                           WooCommerce Maintenance
                         </p>
-                        <p class="mt-1 text-sm text-gray-500">
+                        <p className="mt-1 text-sm text-gray-500">
                           Learn about unlimited WordPress support.
                         </p>
                       </a>
 
                       <a
                         href="/services-ecommerce-development/"
-                        class="-m-3 p-3 block rounded-md hover:bg-gray-50 transition ease-in-out duration-150"
+                        className="-m-3 p-3 block rounded-md hover:bg-gray-50 transition ease-in-out duration-150"
                       >
-                        <p class="text-base font-medium text-gray-900">
+                        <p className="text-base font-medium text-gray-900">
                           eCommerce Development
                         </p>
-                        <p class="mt-1 text-sm text-gray-500">
+                        <p className="mt-1 text-sm text-gray-500">
                           Get all of your questions answered about eCommerce
                           development.
                         </p>
@@ -255,12 +255,12 @@ export default function NavCenter() {
 
                       <a
                         href="/services-b2b-ecommerce/"
-                        class="-m-3 p-3 block rounded-md hover:bg-gray-50 transition ease-in-out duration-150"
+                        className="-m-3 p-3 block rounded-md hover:bg-gray-50 transition ease-in-out duration-150"
                       >
-                        <p class="text-base font-medium text-gray-900">
+                        <p className="text-base font-medium text-gray-900">
                           B2B eCommerce
                         </p>
-                        <p class="mt-1 text-sm text-gray-500">
+                        <p className="mt-1 text-sm text-gray-500">
                           The eCommerce solution for all your B2B needs.
                         </p>
                       </a>
@@ -269,28 +269,28 @@ export default function NavCenter() {
                 </div>
               </div>
               <a
-                class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out"
+                className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out"
                 href="/portfolio/"
                 role="menuitem"
               >
                 Portfolio
               </a>
               <a
-                class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out"
+                className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out"
                 href="/testimonials/"
                 role="menuitem"
               >
                 Testimonials
               </a>
               <a
-                class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out"
+                className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out"
                 href="/pricing/"
                 role="menuitem"
               >
                 Pricing
               </a>
               <a
-                class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out"
+                className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out"
                 href="/about-us/"
                 role="menuitem"
               >
@@ -299,7 +299,7 @@ export default function NavCenter() {
             </div>
             <div>
               <a
-                class="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100 hover:text-indigo-700 focus:outline-none focus:bg-gray-100 focus:text-indigo-700 transition duration-150 ease-in-out"
+                className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100 hover:text-indigo-700 focus:outline-none focus:bg-gray-100 focus:text-indigo-700 transition duration-150 ease-in-out"
                 href="/contact-us/"
                 role="menuitem"
               >
