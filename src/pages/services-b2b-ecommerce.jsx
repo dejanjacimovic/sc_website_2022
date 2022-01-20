87fbab8ebf4435291d75552979f253f98d709eb7
Dejan Jacimovic <dejan@stuntcoders.com>
2022-01-20 10:21:00 +0100

    Fix links and make code prettier

--- src/pages/services-b2b-ecommerce.jsx
+++ src/pages/services-b2b-ecommerce.jsx
@@ -13,8 +13,70 @@ export default function ServicesB2bEcommerce() {
       <div class="relative bg-gray-50 overflow-hidden">
         <div class="hidden sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full">
           <div class="relative h-full max-w-screen-xl mx-auto">
-            <svg class="absolute right-full transform translate-y-1/4 translate-x-1/4 lg:translate-x-1/2" width="404" height="784" fill="none" viewBox="0 0 404 784"><defs><pattern id="f210dbf6-a58d-4871-961e-36d5016a0f49" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse"><rect class="text-gray-200" x="0" y="0" width="4" height="4" fill="currentColor"></rect></pattern></defs><rect width="404" height="784" fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"></rect></svg>
-            <svg class="absolute left-full transform -translate-y-3/4 -translate-x-1/4 md:-translate-y-1/2 lg:-translate-x-1/2" width="404" height="784" fill="none" viewBox="0 0 404 784"><defs><pattern id="5d0dd344-b041-4d26-bec4-8d33ea57ec9b" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse"><rect class="text-gray-200" x="0" y="0" width="4" height="4" fill="currentColor"></rect></pattern></defs><rect width="404" height="784" fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)"></rect></svg>
+            <svg
+              class="absolute right-full transform translate-y-1/4 translate-x-1/4 lg:translate-x-1/2"
+              width="404"
+              height="784"
+              fill="none"
+              viewBox="0 0 404 784"
+            >
+              <defs>
+                <pattern
+                  id="f210dbf6-a58d-4871-961e-36d5016a0f49"
+                  x="0"
+                  y="0"
+                  width="20"
+                  height="20"
+                  patternUnits="userSpaceOnUse"
+                >
+                  <rect
+                    class="text-gray-200"
+                    x="0"
+                    y="0"
+                    width="4"
+                    height="4"
+                    fill="currentColor"
+                  ></rect>
+                </pattern>
+              </defs>
+              <rect
+                width="404"
+                height="784"
+                fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"
+              ></rect>
+            </svg>
+            <svg
+              class="absolute left-full transform -translate-y-3/4 -translate-x-1/4 md:-translate-y-1/2 lg:-translate-x-1/2"
+              width="404"
+              height="784"
+              fill="none"
+              viewBox="0 0 404 784"
+            >
+              <defs>
+                <pattern
+                  id="5d0dd344-b041-4d26-bec4-8d33ea57ec9b"
+                  x="0"
+                  y="0"
+                  width="20"
+                  height="20"
+                  patternUnits="userSpaceOnUse"
+                >
+                  <rect
+                    class="text-gray-200"
+                    x="0"
+                    y="0"
+                    width="4"
+                    height="4"
+                    fill="currentColor"
+                  ></rect>
+                </pattern>
+              </defs>
+              <rect
+                width="404"
+                height="784"
+                fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)"
+              ></rect>
+            </svg>
           </div>
         </div>
         <div class="relative pt-6 pb-12 sm:pb-16 md:pb-20 lg:pb-28 xl:pb-32">
@@ -22,15 +84,27 @@ export default function ServicesB2bEcommerce() {
           <main class="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 xl:mt-28">
             <div class="text-center">
               <h2 class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
-                <span class="mr-1">The <span class="text-indigo-600">eCommerce</span> Solution for<br> All Your <span class="text-indigo-600">B2B</span> Needs.</span>
-
+                <span class="mr-1">
+                  The <span class="text-indigo-600">eCommerce</span> Solution
+                  for
+                  <br /> All Your <span class="text-indigo-600">B2B</span>{' '}
+                  Needs.
+                </span>
               </h2>
-              <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">Over the last decade, we have collaborated with some of the largest Scandinavian fashion brands and some of the most successful ones from Germany to bring to life our enterprise-grade B2B eCommerce platform.</p>
+              <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+                Over the last decade, we have collaborated with some of the
+                largest Scandinavian fashion brands and some of the most
+                successful ones from Germany to bring to life our
+                enterprise-grade B2B eCommerce platform.
+              </p>
               <div class="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
                 <div class="rounded-md shadow">
-                    <a class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10" href="/contact-us.php">
-                        Schedule a demo
-                    </a>
+                  <a
+                    class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
+                    href="/contact/"
+                  >
+                    Schedule a demo
+                  </a>
                 </div>
               </div>
             </div>
@@ -516,7 +590,7 @@ export default function ServicesB2bEcommerce() {
           <div className="mt-8 flex justify-center">
             <div className="inline-flex rounded-md shadow">
               <a
-                href="/contact-us.php"
+                href="/contact/"
                 className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
               >
                 Contact us for a demo
@@ -855,7 +929,7 @@ export default function ServicesB2bEcommerce() {
             <div className="inline-flex rounded-md shadow">
               <a
                 className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
-                href="/contact-us.php"
+                href="/contact/"
               >
                 Contact us
               </a>
@@ -957,7 +1031,7 @@ export default function ServicesB2bEcommerce() {
             </div>
             <a
               className="mt-8 w-full bg-indigo-600 border border-transparent px-5 py-3 inline-flex items-center justify-center text-base leading-6 font-medium rounded-md text-white hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out sm:mt-10 sm:w-auto xl:mt-0"
-              href="/contact-us.php"
+              href="/contact/"
             >
               Get a custom quote
             </a>
