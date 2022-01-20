c7374389e17ca82103e38cb7686df8e03ef1a14d
Dejan Jacimovic <dejan@stuntcoders.com>
2022-01-20 13:26:36 +0100

    Add case study pages wrappers

---
+++ src/pages/case/selekkt.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Link } from 'gatsby';
+import LayoutPage from '../../components/layout_page';
+import NavCenter from '../../components/nav_center';
+
+export default function Selekkt() {
+  return (
+    <LayoutPage>
+    </LayoutPage>
+  );
+}
