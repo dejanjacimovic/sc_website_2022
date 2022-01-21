531cd95a290e470d247eb7fc879891c09c8c0d19
Dejan Jacimovic <dejan@stuntcoders.com>
2022-01-21 08:12:06 +0100

    Use SC favicon

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Layout from '../components/header';
+import favicon from '../images/logo-purple.png'
 
 export default function Header() {
   return (
     <header>
       <title>StuntCoders</title>
+      <link rel="icon" type="image/png" sizes="16x16" href={favicon} />
     </header>
   );
 }
