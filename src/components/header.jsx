706181c80de03550eefc53516d4cdaec9e019de7
Dejan Jacimovic <dejan@stuntcoders.com>
2022-01-21 08:13:29 +0100

    Add more metadata to header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,6 +7,12 @@ export default function Header() {
     <header>
       <title>StuntCoders</title>
       <link rel="icon" type="image/png" sizes="16x16" href={favicon} />
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <meta name="facebook-domain-verification" content="u7n67cd60by0290q0me248tsm49hl0" />
+      <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     </header>
   );
 }
