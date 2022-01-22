a0e521142dbfb7337a06e33d273dcac4d692aed7
Dejan Jacimovic <dejan@stuntcoders.com>
2022-01-23 00:33:18 +0100

    Add first helper file

---
+++ src/helpers/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export function getHeroPhrase() {
+  return 'killing it!';
+}
+
+export function getHeroRegionPhrase() {
+  return 'Europe';
+}
+
+export function getRegionName() {
+  return 'European';
+}
