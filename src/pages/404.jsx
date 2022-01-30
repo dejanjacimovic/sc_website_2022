b1956a4e17c3a1a55fb59c2ffc74cb5805e4aea6
Dejan Jacimovic <dejan@stuntcoders.com>
2022-01-30 12:54:33 +0100

    Fix 404 background image problem

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
 import LayoutPage from '../components/layout_page';
+import lost from '../images/lost.jpeg'
 
 export default function NotFoundPage() {
   let styleDef = {
-    backgroundImage: url(`https://stcdshare.s3.amazonaws.com/EQOnmxU4b4.jpeg`)
+    backgroundImage: "url($lost)"
   }
 
   return (
