0adc7e3330cfe26e7569456d674f81f149862eb3
Dejan Jacimovic <dejan@stuntcoders.com>
2022-01-30 13:06:54 +0100

    Remove favicon from header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Layout from '../components/header';
-import favicon from '../images/logo-purple.png'
 
 export default function Header() {
   return (
     <header>
       <title>StuntCoders</title>
-      <link rel="icon" type="image/png" sizes="16x16" href={favicon} />
       <meta charset="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       <meta name="description" content="" />
