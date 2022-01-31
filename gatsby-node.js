78c9374a2ec8b054bcc079f8f7d94c22fa0e3b36
Dejan Jacimovic <dejan@stuntcoders.com>
2022-01-31 14:43:02 +0100

    Add gatby-node.js to provide additional data on load-time

---
+++ gatsby-node.js
@@ -0,0 +1,39 @@
+const fetch = require('node-fetch');
+
+exports.sourceNodes = async ({
+  actions: { createNode },
+  createContentDigest
+}) => {
+  const response = await fetch('https://api.ipify.org?format=jsonp&callback=getIP');
+  const data = await response.json();
+  data.response.docs.forEach((item) => {
+    createNode({
+      ...item,
+      id: item._id,
+      internal: {
+        type: 'CustomerIp',
+        contentDigest: createContentDigest(item)
+      }
+    });
+  });
+};
+
+
+exports.sourceNodes = async ({
+  actions: { createNode },
+  createContentDigest
+}) => {
+  const response = await fetch('http://api.ipstack.com/178.222.19.167?access_key=ecb285d703ed9506024a975b75fc7b7b');
+  const data = await response.json();
+  console.log(data.response);
+  // data.response.docs.forEach((item) => {
+  //   createNode({
+  //     ...item,
+  //     id: item._id,
+  //     internal: {
+  //       type: 'Country',
+  //       contentDigest: createContentDigest(item)
+  //     }
+  //   });
+  // });
+};
