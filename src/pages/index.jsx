74f61a857731a8ba9f101244893a29d5588f7f78
Dejan Jacimovic <dejan@stuntcoders.com>
2022-02-02 21:19:03 +0100

    Fix svg attribute name

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -138,7 +138,7 @@ export default function Home() {
                           stroke="currentColor"
                         >
                           <path
-                            stroke-linecap="round"
+                            strokeLinecap="round"
                             strokeLinejoin="round"
                             strokeWidth="2"
                             d="M13 10V3L4 14h7v7l9-11h-7z"
@@ -169,7 +169,7 @@ export default function Home() {
                           stroke="currentColor"
                         >
                           <path
-                            stroke-linecap="round"
+                            strokeLinecap="round"
                             strokeLinejoin="round"
                             strokeWidth="2"
                             d="M7 11.5V14m0-2.5v-6a1.5 1.5 0 113 0m-3 6a1.5 1.5 0 00-3 0v2a7.5 7.5 0 0015 0v-5a1.5 1.5 0 00-3 0m-6-3V11m0-5.5v-1a1.5 1.5 0 013 0v1m0 0V11m0-5.5a1.5 1.5 0 013 0v3m0 0V11"
@@ -201,7 +201,7 @@ export default function Home() {
                           stroke="currentColor"
                         >
                           <path
-                            stroke-linecap="round"
+                            strokeLinecap="round"
                             strokeLinejoin="round"
                             strokeWidth="2"
                             d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
@@ -320,7 +320,7 @@ export default function Home() {
                             stroke="currentColor"
                           >
                             <path
-                              stroke-linecap="round"
+                              strokeLinecap="round"
                               strokeLinejoin="round"
                               strokeWidth="2"
                               d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"
@@ -353,7 +353,7 @@ export default function Home() {
                             stroke="currentColor"
                           >
                             <path
-                              stroke-linecap="round"
+                              strokeLinecap="round"
                               strokeLinejoin="round"
                               strokeWidth="2"
                               d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
@@ -997,7 +997,7 @@ export default function Home() {
                     aria-hidden="true"
                   >
                     <path
-                      stroke-linecap="round"
+                      strokeLinecap="round"
                       strokeLinejoin="round"
                       strokeWidth="2"
                       d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
@@ -1029,7 +1029,7 @@ export default function Home() {
                     aria-hidden="true"
                   >
                     <path
-                      stroke-linecap="round"
+                      strokeLinecap="round"
                       strokeLinejoin="round"
                       strokeWidth="2"
                       d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
@@ -1060,7 +1060,7 @@ export default function Home() {
                     aria-hidden="true"
                   >
                     <path
-                      stroke-linecap="round"
+                      strokeLinecap="round"
                       strokeLinejoin="round"
                       strokeWidth="2"
                       d="M13 10V3L4 14h7v7l9-11h-7z"
