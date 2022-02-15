c0a002ac2d91cc9eda16cd25fbf75512f56e85f3
Dejan Jacimovic <dejan@stuntcoders.com>
2022-02-15 17:32:23 +0100

    Prettify acessibility test

--- cypress/e2e/accessibility.test.js
+++ cypress/e2e/accessibility.test.js
@@ -1,22 +1,22 @@
 /// <reference types="Cypress" />
 
-describe("Accessibility tests", () => {
+describe('Accessibility tests', () => {
   beforeEach(() => {
-    cy.visit("/").get("main").injectAxe()
-  })
-  it("Has no detectable accessibility violations on load", () => {
-    cy.checkA11y()
-  })
-  it("Navigates to Pricing and check for accessibility violations", () => {
+    cy.visit('/').get('main').injectAxe();
+  });
+  it('Has no detectable accessibility violations on load', () => {
+    cy.checkA11y();
+  });
+  it('Navigates to Pricing and check for accessibility violations', () => {
     cy.findByText(/Pricing/i)
       .click()
-      .checkA11y()
-  })
-  it("Focuses on the footer link and asserts its attributes", () => {
-    cy.findAllByText("Careers").focus()
+      .checkA11y();
+  });
+  it('Focuses on the footer link and asserts its attributes', () => {
+    cy.findAllByText('Careers').focus();
     cy.focused()
-      .should("have.text", "Careers")
-      .should("have.attr", "href", "https://stuntcoders.careers")
-      .should("not.have.css", "outline-width", "0px")
-  })
-})
+      .should('have.text', 'Careers')
+      .should('have.attr', 'href', 'https://stuntcoders.careers/')
+      .should('not.have.css', 'outline-width', '0px');
+  });
+});
