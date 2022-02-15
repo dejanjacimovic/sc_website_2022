bbe461c6750060e8637cdb335e62825249cc51f6
Dejan Jacimovic <dejan@stuntcoders.com>
2022-02-15 14:34:01 +0100

    Test footer for pricing link and submenu title

--- src/components/__tests__/footer.jsx
+++ src/components/__tests__/footer.jsx
@@ -5,9 +5,15 @@ import Footer from '../footer';
 
 describe('Footer', () => {
   it('renders correctly', () => {
-    const tree = renderer
+    const submenuTitle = renderer
       .create(<Footer h4="Company" />)
       .toJSON();
-    expect(tree).toMatchSnapshot();
+
+    const pricingLink = renderer
+      .create(<Link to="/pricing/" />)
+      .toJSON();
+
+    expect(submenuTitle).toMatchSnapshot();
+    expect(pricingLink).toMatchSnapshot();
   });
 });
