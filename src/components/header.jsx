1da0f8ce4bcb4d6240d0e526b1858d51bd251ba5
Dejan Jacimovic <dejan@stuntcoders.com>
2022-02-15 11:12:58 +0100

    Use Helemt instead of header to avoid errors with rendering

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import Layout from '../components/header';
+import { Helmet } from 'react-helmet';
 
 export default function Header() {
   return (
-    <header>
+    <Helmet>
       <title>StuntCoders</title>
-      <meta charset="utf-8" />
+      <meta charSet="utf-8" />
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, shrink-to-fit=no"
       />
-      <meta name="description" content="" />
-      <meta name="author" content="" />
+      <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       <meta
         name="facebook-domain-verification"
         content="u7n67cd60by0290q0me248tsm49hl0"
       />
-      <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-    </header>
+    </Helmet>
   );
 }
