021f71020fc15d90a887c3dc2cfd28eab248f26e
Dejan Jacimovic <dejan@stuntcoders.com>
2022-02-18 09:19:54 +0100

    cleanup gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -25,15 +25,5 @@ exports.sourceNodes = async ({
 }) => {
   const response = await fetch('http://api.ipstack.com/178.222.19.167?access_key=ecb285d703ed9506024a975b75fc7b7b');
   const data = await response.json();
-  console.log(data.response);
-  // data.response.docs.forEach((item) => {
-  //   createNode({
-  //     ...item,
-  //     id: item._id,
-  //     internal: {
-  //       type: 'Country',
-  //       contentDigest: createContentDigest(item)
-  //     }
-  //   });
-  // });
 };
+
