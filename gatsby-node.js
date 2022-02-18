b3abe8ca2752e697c2b343e09775ff3b113eaf30
Dejan Jacimovic <dejan@stuntcoders.com>
2022-02-18 09:24:15 +0100

    Add two redirects for testing purposes

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,9 +2,11 @@ const fetch = require('node-fetch');
 
 exports.sourceNodes = async ({
   actions: { createNode },
-  createContentDigest
+  createContentDigest,
 }) => {
-  const response = await fetch('https://api.ipify.org?format=jsonp&callback=getIP');
+  const response = await fetch(
+    'https://api.ipify.org?format=jsonp&callback=getIP'
+  );
   const data = await response.json();
   data.response.docs.forEach((item) => {
     createNode({
@@ -12,18 +14,34 @@ exports.sourceNodes = async ({
       id: item._id,
       internal: {
         type: 'CustomerIp',
-        contentDigest: createContentDigest(item)
-      }
+        contentDigest: createContentDigest(item),
+      },
     });
   });
 };
 
-
 exports.sourceNodes = async ({
   actions: { createNode },
-  createContentDigest
+  createContentDigest,
 }) => {
-  const response = await fetch('http://api.ipstack.com/178.222.19.167?access_key=ecb285d703ed9506024a975b75fc7b7b');
+  const response = await fetch(
+    'http://api.ipstack.com/178.222.19.167?access_key=ecb285d703ed9506024a975b75fc7b7b'
+  );
   const data = await response.json();
 };
 
+exports.createPages = async ({ graphql, actions }) => {
+  const { createRedirect } = actions;
+
+  createRedirect({
+    fromPath: `/contact-us.php`,
+    toPath: `/contact`,
+    statusCode: 301,
+  });
+
+  createRedirect({
+    fromPath: `/blog/*`,
+    toPath: `/`,
+    statusCode: 301,
+  });
+};
