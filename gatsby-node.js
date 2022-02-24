868fd2c19654acf0d6e3572d07da2011229f915a
Dejan Jacimovic <dejan@stuntcoders.com>
2022-02-24 20:11:03 +0100

    Remove unwanted commit code

--- gatsby-node.js
+++ gatsby-node.js
@@ -25,26 +25,6 @@ exports.sourceNodes = async ({
   createContentDigest,
 }) => {
   const response = await fetch(
-    'https://kurs.resenje.org/api/v1/currencies/eur/rates/today'
-  );
-  const data = await response.json();
-  data.response.docs.forEach((item) => {
-    createNode({
-      ...item,
-      id: item._id,
-      internal: {
-        type: 'exchangeRate',
-        contentDigest: createContentDigest(item),
-      },
-    });
-  });
-};
-
-exports.sourceNodes = async ({
-  actions: { createNode },
-  createContentDigest,
-}) => {
-  const response = await fetch(
     'http://api.ipstack.com/178.222.19.167?access_key=ecb285d703ed9506024a975b75fc7b7b'
   );
   const data = await response.json();
