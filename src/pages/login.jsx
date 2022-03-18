1fccec9ba876b76d4d43d1299bdf6de58cc5473a
Dejan Jacimovic <dejan@stuntcoders.com>
2022-03-19 00:19:19 +0100

    Remove auth entirely

--- src/pages/login.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { isAuthenticated } from '../utils/auth';
-import LayoutPage from '../components/layout_page';
-
-export default function Invoice() {
-  if (!isAuthenticated()) {
-    return <LayoutPage>Redirecting you to the login screen...</LayoutPage>;
-  }
-}
