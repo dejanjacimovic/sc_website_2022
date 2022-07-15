d06fd298494f592651e046c4785823787237acda
Dejan Jacimovic <dejan@stuntcoders.com>
2022-07-15 19:40:29 +0200

    Update Marketplaces service page

--- src/pages/services/marketplace-management.jsx
+++ src/pages/services/marketplace-management.jsx
@@ -136,7 +136,7 @@ export default function MarketplaceManagement() {
                   <p className="text-center text-sm font-semibold uppercase tracking-wide text-gray-500">
                     Would you sell on any of these?
                   </p>
-                  <div className="mt-6 grid grid-cols-2 gap-6 md:grid-cols-6 lg:grid-cols-6">
+                  <div className="mt-6 grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
                     <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
                       <img className="h-12" src={aboutYouImg} alt="AboutYou" />
                     </div>
